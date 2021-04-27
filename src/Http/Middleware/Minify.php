21f37bacaa4bbe9943184ce6cf597c99e35ee31e
Giovanni Buffa <tech@giovannibuffa.it>
2021-04-27 14:50:48 +0200

    Minify HTML from tiny html minifier and set requirements for Statamic from version 3.0 to upper

--- src/Http/Middleware/Minify.php
+++ src/Http/Middleware/Minify.php
@@ -7,6 +7,7 @@ namespace Gioppy\StatamicSimpleMinifyHtml\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Minifier\TinyMinify;
 
 class Minify {
 
@@ -25,7 +26,7 @@ class Minify {
 
     if ($this->isResponse($response) && $this->isHtml($response) && env('MINIFY', false)) {
       $content = $response->getContent();
-      $content = preg_replace(['/\>[^\S ]+/s', '/[^\S ]+\</s', '/(\s)+/s', '/<!--(.|\s)*?-->/'], ['>', '<', '\\1'], $content);
+      $content = TinyMinify::html($content);
 
       $response->setContent($content);
     }
